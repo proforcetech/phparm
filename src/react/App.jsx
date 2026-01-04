bcff1c696151b7ee88fde1110230eb00bbb6a047
proforcetech <aesquivel@cytext.biz>
2026-01-03 19:55:41 -0500

    Load global styles for React app

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 
-import ToastDemo from './components/ToastDemo'
 import { router } from './router'
 
 export default function App() {
   return (
     <>
-      <ToastDemo />
       <RouterProvider router={router} />
     </>
   )
